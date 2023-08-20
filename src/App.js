f46cc7c4fb5a1370743271eb37c9b92bc57d80d9
Manuel Sebastian De Los Santos <manuelsebastiandls@gmail.com>
2023-08-20 01:13:35 -0300

    Empece el proyecto de aprender reacts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Greeting from './Components/pure/greeting';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          {/*Componente propio greeting.jsx */}
+          <Greeting name="Manuel"></Greeting>
         <a
           className="App-link"
           href="https://reactjs.org"
